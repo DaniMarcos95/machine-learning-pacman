0231a81bd8d01b82d98f4b0750a3f1f101816ff7
DaniMarcos95 <danimarcosmazon@gmail.com>
2018-12-26 14:43:37 +0100

    Small modification

--- Deep Q Learning/pacmanDQN_Agents.py
+++ Deep Q Learning/pacmanDQN_Agents.py
@@ -33,19 +33,17 @@ params = {
     'save_interval' : 10000, 
 
     # Training parameters
-    'train_start': 5000,    # Episodes before training starts
+    'train_start': 15000,    # Episodes before training starts
     'batch_size': 32,       # Replay memory batch size
     'mem_size': 100000,     # Replay memory size
 
     'discount': 0.99,       # Discount rate (gamma value)
-    'lr': .0002,            # Learning reate
-    # 'rms_decay': 0.99,      # RMS Prop decay (switched to adam)
-    # 'rms_eps': 1e-6,        # RMS Prop epsilon (switched to adam)
-
+    'lr': .00025,            # Learning reate
+    
     # Epsilon value (epsilon-greedy)
     'eps': 1.0,             # Epsilon start value
-    'eps_final': 0.05,       # Epsilon end value
-    'eps_step': 100000       # Epsilon steps between start and end (linear)
+    'eps_final': 0.1,       # Epsilon end value
+    'eps_step': 1000000       # Epsilon steps between start and end (linear)
 }                     
 
 
@@ -288,112 +286,6 @@ class PacmanDQN(game.Agent):
         for i in range(len(actions)):                                           
             actions_onehot[i][int(actions[i])] = 1      
         return actions_onehot   
-
-    # def mergeStateMatrices(self, stateMatrices):
-    #     """ Merge state matrices to one state tensor """
-    #     stateMatrices = np.swapaxes(stateMatrices, 0, 2)
-    #     total = np.zeros((7, 7))
-    #     for i in range(len(stateMatrices)):
-    #         total += (i + 1) * stateMatrices[i] / 6
-    #     return total
-
-    # def getStateMatrices(self, state):
-    #     """ Return wall, ghosts, food, capsules matrices """ 
-    #     def getWallMatrix(state):
-    #         """ Return matrix with wall coordinates set to 1 """
-    #         width, height = state.data.layout.width, state.data.layout.height
-    #         grid = state.data.layout.walls
-    #         matrix = np.zeros((height, width), dtype=np.int8)
-    #         for i in range(grid.height):
-    #             for j in range(grid.width):
-    #                 # Put cell vertically reversed in matrix
-    #                 cell = 1 if grid[j][i] else 0
-    #                 matrix[-1-i][j] = cell
-    #         return matrix
-
-    #     def getPacmanMatrix(state):
-    #         """ Return matrix with pacman coordinates set to 1 """
-    #         width, height = state.data.layout.width, state.data.layout.height
-    #         matrix = np.zeros((height, width), dtype=np.int8)
-
-    #         for agentState in state.data.agentStates:
-    #             if agentState.isPacman:
-    #                 pos = agentState.configuration.getPosition()
-    #                 cell = 1
-    #                 matrix[-1-int(pos[1])][int(pos[0])] = cell
-
-    #         return matrix
-
-    #     def getGhostMatrix(state):
-    #         """ Return matrix with ghost coordinates set to 1 """
-    #         width, height = state.data.layout.width, state.data.layout.height
-    #         matrix = np.zeros((height, width), dtype=np.int8)
-
-    #         for agentState in state.data.agentStates:
-    #             if not agentState.isPacman:
-    #                 if not agentState.scaredTimer > 0:
-    #                     pos = agentState.configuration.getPosition()
-    #                     cell = 1
-    #                     matrix[-1-int(pos[1])][int(pos[0])] = cell
-
-    #         return matrix
-
-    #     def getScaredGhostMatrix(state):
-    #         """ Return matrix with ghost coordinates set to 1 """
-    #         width, height = state.data.layout.width, state.data.layout.height
-    #         matrix = np.zeros((height, width), dtype=np.int8)
-
-    #         for agentState in state.data.agentStates:
-    #             if not agentState.isPacman:
-    #                 if agentState.scaredTimer > 0:
-    #                     pos = agentState.configuration.getPosition()
-    #                     cell = 1
-    #                     matrix[-1-int(pos[1])][int(pos[0])] = cell
-
-    #         return matrix
-
-    #     def getFoodMatrix(state):
-    #         """ Return matrix with food coordinates set to 1 """
-    #         width, height = state.data.layout.width, state.data.layout.height
-    #         grid = state.data.food
-    #         matrix = np.zeros((height, width), dtype=np.int8)
-
-    #         for i in range(grid.height):
-    #             for j in range(grid.width):
-    #                 # Put cell vertically reversed in matrix
-    #                 cell = 1 if grid[j][i] else 0
-    #                 matrix[-1-i][j] = cell
-
-    #         return matrix
-
-    #     def getCapsulesMatrix(state):
-    #         """ Return matrix with capsule coordinates set to 1 """
-    #         width, height = state.data.layout.width, state.data.layout.height
-    #         capsules = state.data.layout.capsules
-    #         matrix = np.zeros((height, width), dtype=np.int8)
-
-    #         for i in capsules:
-    #             # Insert capsule cells vertically reversed into matrix
-    #             matrix[-1-i[1], i[0]] = 1
-
-    #         return matrix
-
-    #     # Create observation matrix as a combination of
-    #     # wall, pacman, ghost, food and capsule matrices
-    #     # width, height = state.data.layout.width, state.data.layout.height 
-    #     width, height = self.params['width'], self.params['height']
-    #     observation = np.zeros((6, height, width))
-
-    #     observation[0] = getWallMatrix(state)
-    #     observation[1] = getPacmanMatrix(state)
-    #     observation[2] = getGhostMatrix(state)
-    #     observation[3] = getScaredGhostMatrix(state)
-    #     observation[4] = getFoodMatrix(state)
-    #     observation[5] = getCapsulesMatrix(state)
-
-    #     observation = np.swapaxes(observation, 0, 2)
-
-    #     return observation
 
     def registerInitialState(self, state): # inspects the starting state
 
