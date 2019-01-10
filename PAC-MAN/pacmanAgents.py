b4b7963566512bc03aedc62bb414b6b928befa63
Superkebabbie <joostdootnkamp@hotmail.com>
2019-01-10 11:08:09 +0100

    basic features

--- PAC-MAN/pacmanAgents.py
+++ PAC-MAN/pacmanAgents.py
@@ -17,6 +17,7 @@ from game import Agent
 import random
 import game
 import util
+import Queue
 
 class LeftTurnAgent(game.Agent):
     "An agent that turns left at every opportunity"
@@ -50,3 +51,104 @@ class GreedyAgent(Agent):
 
 def scoreEvaluation(state):
     return state.getScore()
+    
+#------------ JOOSTCODE --------------#
+
+#OBJECTIVES/Features index:
+
+# 1) number of pills to remaining to eat
+# 2) number of pills already eaten
+# 3) number of ghost
+# 4) distance from ghost
+# 5) distance to nearest pill
+# 6) safe paths
+# 7) distance to big pill/fruit
+
+class TestAgent(game.Agent):
+    """ KeyboardAgent that tells you a lot of shit about the game state """
+    # NOTE: Arrow keys also work.
+    WEST_KEY  = 'a'
+    EAST_KEY  = 'd'
+    NORTH_KEY = 'w'
+    SOUTH_KEY = 's'
+    STOP_KEY = 'q'
+
+    def __init__( self, index = 0 ):
+        self.lastMove = Directions.STOP
+        self.index = index
+        self.keys = []
+        self.startfood = None #in the very first state, log how much food existed to be able to track how much you ate
+        
+    def __mazeCrawler(self, state):
+        #Apply BFS to the maze to find the nearest food pill
+        search = Queue.Queue()
+        search.put((int(state.getPacmanPosition()[0]),int(state.getPacmanPosition()[1])))#start state cannot contain pill; checking first is redundant but easier
+        hit = []
+        explored = []
+        while not hit:
+            (x,y) = search.get()
+            explored.append((x,y))
+            print(x,y)
+            if state.hasFood(x,y):
+                hit.append((x,y))
+                print("hit!")
+            else:
+                for newx,newy in [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]:
+                    if not state.hasWall(newx,newy) and (newx,newy) not in explored:
+                        search.put((newx,newy))
+        while not search.empty():
+            (x,y) = search.get()
+            if state.hasFood(x,y): hit.append((x,y))
+        return hit
+        
+    def __getFeatures(self, state):
+        print(state)
+        print(vars(state.data))
+        features = {}
+        features['foodRemaining'] = state.getNumFood()
+        if self.startfood == None: self.startfood = state.getNumFood()
+        features['foodEaten'] = self.startfood - state.getNumFood()
+        features['numGhosts'] = len(state.getGhostStates())
+        pacpos = state.getPacmanPosition()
+        features['distanceToGhosts'] = [(x-pacpos[0],y-pacpos[1]) for (x,y) in state.getGhostPositions()]
+        #TODO: nearest pill requires a breath first search maze crawler, preferably finishing a certain distance
+        features['nearestPill'] = self.__mazeCrawler(state)
+        #TODO: if maze crawler is done, apply that to ghosts as well, because now it sees through walls
+        features['distanceToCapsules'] = [(x-pacpos[0],y-pacpos[1]) for (x,y) in state.getCapsules()]
+        
+        return features
+        
+
+    def getAction( self, state):
+        features = self.__getFeatures(state)
+        print(features)
+        
+        from graphicsUtils import keys_waiting
+        from graphicsUtils import keys_pressed
+        keys = keys_waiting() + keys_pressed()
+        if keys != []:
+            self.keys = keys
+
+        legal = state.getLegalActions(self.index)
+        move = self.getMove(legal)
+
+        if move == Directions.STOP:
+            # Try to move in the same direction as before
+            if self.lastMove in legal:
+                move = self.lastMove
+
+        if (self.STOP_KEY in self.keys) and Directions.STOP in legal: move = Directions.STOP
+
+        if move not in legal:
+            move = random.choice(legal)
+
+        self.lastMove = move
+        return move
+
+    def getMove(self, legal):
+        move = Directions.STOP
+        if   (self.WEST_KEY in self.keys or 'Left' in self.keys) and Directions.WEST in legal:  move = Directions.WEST
+        if   (self.EAST_KEY in self.keys or 'Right' in self.keys) and Directions.EAST in legal: move = Directions.EAST
+        if   (self.NORTH_KEY in self.keys or 'Up' in self.keys) and Directions.NORTH in legal:   move = Directions.NORTH
+        if   (self.SOUTH_KEY in self.keys or 'Down' in self.keys) and Directions.SOUTH in legal: move = Directions.SOUTH
+        return move
